b3d44ca670bc6491a5ecbab236f553c6e9797e64
Jonathan Ho <johnnyho82@gmail.com>
2020-01-21 19:03:21 -0500

    Commit before pull

---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/404.js': require('/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/404.js').default,
+'/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/about.js': require('/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/about.js').default,
+'/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/blog.js': require('/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/blog.js').default,
+'/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/index.js': require('/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/index.js').default,
+'/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/containers/Post': require('/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/home/pi/Desktop/ReactStatic/JAMstack_ReactStaticNetlify/src/pages/404.js'
+
